ef428fc3d6322153417a1eadbe33eed226e0162b
Eunji Kim <eunk.angie@outlook.com>
2019-10-27 19:27:38 +0900

    change the port setting

--- API/index.js
+++ API/index.js
@@ -6,7 +6,6 @@ const venues = require('./routes/venues');
 app.use(express.json());
 app.use('/v1/venues', venues);
 
-const port = parseInt(process.env.PORT || '3000', 10);
-app.set('port',port);
+const port = process.env.port || process.env.PORT || 3000;
 
-app.listen(app.get('port'), function() { console.log(`Listening on port ${app.get('port')}`)});
+app.listen(port, ()=> { console.log(`Listening on port ${port}`)});
